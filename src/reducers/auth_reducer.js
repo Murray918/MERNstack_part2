3429c31b4ec30b25e60ed1d5a8a985eeca903cb4
Andrew Murray <darbyguitars@gmail.com>
2018-02-07 11:40:35 -0600

    chasing errors

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,5 +1,12 @@
+import {
+	AUTH_USER,
+	UNAUTH_USER,
+	AUTH_ERROR,
+	FETCH_MESSAGE
+} from '../actions/types';
+
 export default function(state = {}, action) {
-	switch (atcion.type) {
+	switch (action.type) {
 		case AUTH_USER:
 			return { ...state, authenticated: true };
 		case UNAUTH_USER:
